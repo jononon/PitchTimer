6060351f98e00e713531c6ea1059efbffd0977fa
Jonathan Damico <jj_damico@yahoo.com>
2016-06-14 17:29:30 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var active = false;
-    var seconds = 60;
+    var seconds = 15;
     
     myNewWindow = window.open("timer.html");
     
     setInterval(function(){ 
         $('#time').text(seconds);
         if(seconds<=10)
-            $("p").css("color", "rgb(198, 27, 23)");
+            $("#time").css("color", "rgb(198, 27, 23)");
         if(active && seconds>0)
             seconds--;
     }, 1000);
