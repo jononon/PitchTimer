2e86bcce34d0ea3d9d583042c31358e5f439c142
Jonathan Damico <jj_damico@yahoo.com>
2017-06-17 10:06:16 -0700

    Update js.js

--- js.js
+++ js.js
@@ -6,10 +6,13 @@ $(document).ready(function () {
     myNewWindow = window.open("timer.html");
     
     setInterval(function(){
-        if(seconds>60)
+        if(seconds>60) {
             $('#time').text(""+parseInt(seconds/60)+":"+((seconds%60<10)?("0"+seconds%60):(seconds%60)));
-        else
+            myNewWindow.$('#time').text(""+parseInt(seconds/60)+":"+((seconds%60<10)?("0"+seconds%60):(seconds%60)));
+        } else {
             $('#time').text(seconds);
+            myNewWindow.$('#time').text(seconds);
+        }
             
         if(seconds<=10)
             $("#time").css("color", "rgb(198, 27, 23)");
